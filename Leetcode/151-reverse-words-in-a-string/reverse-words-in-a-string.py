59ce9e7442f9f1579f1552a458d9b7b07a9cad81
Pranamya Jain <146780116+pranamyajainn@users.noreply.github.com>
2025-03-30 21:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.86%) - LeetSync

--- Leetcode/151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ Leetcode/151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,16 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        stack = [] 
-        word = ""
-        for ch in s: 
-            #if not space we need to build the word
-            if ch!= ' ':
-                word += ch
-            elif word:
-                stack.append(word)
-                word= ""
-        #pushing last word(without space)
-        if word:
-            stack.append(word)
-        #reverse the stack and rebuild string 
-        return " ".join(stack[::-1])
+        #split by spaces (automatically removes spaces)
+        words = s.split()
+        #reverse the word
+        words.reverse()
+        #join back with single space
+        return " ".join(words)
